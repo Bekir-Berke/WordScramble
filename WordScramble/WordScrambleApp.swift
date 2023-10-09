5dd5af45caf176f468badb9903f80e86823fd4db
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-10-09 19:50:54 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bekir Berke Yılmaz on 9.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
